4da989b726c385c131d62b75d1360bf74c96ee67
Mike Aleksa <mtaleksa1@gmail.com>
2022-01-20 06:18:03 +0000

    additional layout with flexbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Headshot from '../public/headshot-web.png'
-import styles from '../styles/Home.module.css'
 import rotate from '../styles/Rotate.module.css'
-// import animate from 'animate.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Michael Aleksa • Software Engineer</title>
         <meta
@@ -18,66 +16,47 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.wrapper}>
-          <h1 className={styles.title}>Hi, I&apos;m Michael.</h1>
-          <div className={rotate['rw-wrapper']}>
-            <p
-              className={`${styles.description} ${rotate['rw-words']} ${rotate['rw-words1']}`}
-            >
-              <span>Full-Stack Web Development</span>
-              <span>Machine Learning</span>
-              <span>DevOps</span>
-              <span>Cloud Engineering</span>
-            </p>
+      <main className="main">
+        <div className="wrapper primary-bg">
+          <div>
+            <h1 className="title">Hi, I&apos;m Michael.</h1>
+            <div className={rotate['rw-wrapper']}>
+              <p
+                className={`description ${rotate['rw-words']} ${rotate['rw-words1']}`}
+              >
+                <span>Full-Stack Web Development</span>
+                <span>Machine Learning</span>
+                <span>DevOps</span>
+                <span>Cloud Engineering</span>
+              </p>
+            </div>
           </div>
-          <Image
-            className={styles.headshot}
-            src={Headshot}
-            alt="Michael Aleksa"
-            height={500}
-            width={500}
-          />
+          <div className="center">
+            <Image
+              className="headshot"
+              src={Headshot}
+              alt="Michael Aleksa"
+              // layout="responsive"
+              height={450}
+              width={450}
+            />
+          </div>
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={'wrapper center secondary-bg'}>
+          <h2 className="description">Experience will go here</h2>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="footer primary-bg">
+        <span>Built with Next.js and Typescript</span>
+        <span className="hidden">&nbsp;--&nbsp;</span>
         <a
           href="https://github.com/MikeAleksa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Built with Next.js and Typescript -- See source code on Github
+          See source code on Github
         </a>
       </footer>
     </div>
