f2a97304bc215b253a40366e18c0d4a2d0243f23
Mike Aleksa <mtaleksa1@gmail.com>
2022-01-21 17:29:17 +0000

    add additional footer styling and animations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,29 @@
 const Footer = () => (
   <footer className="footer inverted">
     <div className={'contact-text footer-grid'}>
-      <a>LINKEDIN</a>
-      <a>ANGELLIST</a>
-      <a>GITHUB</a>
+      <a>AngelList</a>
+      <a>GitHub</a>
+      <a>LinkedIn</a>
     </div>
     <div>
       <p
         className="footer-text"
         style={{ margin: '1rem 0 0 0', letterSpacing: '0.1rem' }}
       >
-        &apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;&apos;
+        &apos;&apos;&apos;
       </p>
     </div>
     <div className="footer-text">
       <p>
-        Built with Next.js and Typescript. See the source code on&nbsp;
+        Built with Next.js and Typescript. See the&nbsp;
         <a
           href="https://github.com/MikeAleksa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          source code
         </a>
+        &nbsp;on Github
       </p>
       <p className="footer-accent-text">
         <a>Hire Me</a>
